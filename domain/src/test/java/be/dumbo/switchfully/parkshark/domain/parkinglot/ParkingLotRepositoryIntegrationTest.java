409fec572eb5771ef68ee22d0e864afd61c4804f
El Mahraoui Omar (100) <omar.elmahraoui@cmc.be>
2018-04-16 16:51:17 +0200

    working on creating a parkingLotTestBuilder to be able to run tests for creating a parkingLot

--- domain/src/test/java/be/dumbo/switchfully/parkshark/domain/parkinglot/ParkingLotRepositoryIntegrationTest.java
+++ domain/src/test/java/be/dumbo/switchfully/parkshark/domain/parkinglot/ParkingLotRepositoryIntegrationTest.java
@@ -1,3 +1,4 @@
+/*
 package be.dumbo.switchfully.parkshark.domain.parkinglot;
 
 import be.dumbo.switchfully.parkshark.infrastructure.IntegrationTest;
@@ -8,11 +9,14 @@ public class ParkingLotRepositoryIntegrationTest extends IntegrationTest{
     @Autowired
     private ParkingLotRepository parkingLotRepository;
 
-    /*@Test
+    */
+/*@Test
     public void save_givenAParkingLot_thenSaveTheParkingLotInDatabaseAndReturnTheDivisionWithTheId() {
         //GIVEN
         ParkingLot parkingLot = parkingLot()
 
-    }*/
+    }*//*
+
 
 }
+*/
